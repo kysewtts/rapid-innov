0bae6f77c33463587d658a44c5190a9d45e9137c
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2022-02-21 21:21:44 +0530

    Added env vars

--- db/queries.js
+++ db/queries.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'ujjwalprakash',
-  host: 'localhost',
-  database: 'rapid',
-  password: '',
+  user: process.env.PG_USER,
+  host: process.env.DB_HOST,
+  database: process.env.PG_DEFAULT_DB,
+  password: process.env.PG_PASSWORD,
   port: process.env.DB_PORT,
 });
 
