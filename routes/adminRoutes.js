2bf6a674024e079ed02b0cb78f43d02aa4387be1
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2022-02-22 08:31:04 +0530

    Added functionality for admin to search players through name and sport

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,6 +4,7 @@ const adminControllers = require('../controllers/adminControllers');
 
 const router = express.Router();
 
+router.get('/:fname/:lname/:sport', adminControllers.getByNameAndSport);
 router.get('/', adminControllers.getPlayers);
 
 module.exports = router;
--- routes/coachRoutes.js
+++ routes/coachRoutes.js
@@ -4,7 +4,10 @@ const coachControllers = require('../controllers/coachControllers');
 
 const router = express.Router();
 
-router.get('/:coachId/:fname/:lname/:sport', coachControllers.getNameAndSport);
+router.get(
+  '/:coachId/:fname/:lname/:sport',
+  coachControllers.getByNameAndSport
+);
 router.get('/:coachId', coachControllers.getPlayers);
 router.post('/:coachId', coachControllers.postPlayers);
 
